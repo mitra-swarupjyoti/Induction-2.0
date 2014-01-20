2d1a103addda893a2ae580691fbfee6cd9a5f9c6
unknown <Nandan Mankad@FIR000095520002.ads.mckinsey.com>
2014-01-20 06:29:28 -0500

    Induction 2.0 App initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Induction20::Application.config.secret_key_base = '3129ac7de48782ce458fb6351d85fb4348fd220a076b54012577080d72b893e79f07edcae39faad4b8c9937d097c79ef113d42b10c8a0d64653187a00a3e803c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Induction20::Application.config.session_store :cookie_store, key: '_Induction-2_0_session'
